7af1625207d4934c5dd09fc848b3ebb2320620a6
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2025-03-10 16:21:27 +0000

    fix ui etc

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,54 +1,41 @@
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { Color, CircleIconButtonProps } from "@/types/link";
 
-const buttonLinkStye: { [key in Color]: string } = {
-  blue: "inline-block rounded-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-800 disabled:bg-blue-200",
-  red: "inline-block rounded-lg px-4 py-2 bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-red-800 disabled:bg-red-200",
-  green:
-    "inline-block rounded-lg px-4 py-2 bg-green-500 text-white hover:bg-green-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-green-800 disabled:bg-green-200",
-  orange:
-    "inline-block rounded-lg px-4 py-2 bg-orange-500 text-white hover:bg-orange-700 focus:bg-orange-500 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-orange-800 disabled:bg-orange-200",
-};
+import {
+  type Color,
+  type ButtonProps,
+  type LoadingButtonProps,
+  type IconButtonProps,
+  type CircleIconButtonProps,
+} from "@/types/ui";
 
-const buttonFullLinkStye: { [key in Color]: string } = {
-  blue: "inline-block w-full rounded-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-800 disabled:bg-blue-200",
-  red: "inline-block w-full rounded-lg px-4 py-2 bg-red-500 text-white hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-red-800 disabled:bg-red-200",
-  green:
-    "inline-block w-full rounded-lg px-4 py-2 bg-green-500 text-white hover:bg-green-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-green-800 disabled:bg-green-200",
-  orange:
-    "inline-block w-full rounded-lg px-4 py-2 bg-orange-500 text-white hover:bg-orange-700 focus:bg-orange-500 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-orange-800 disabled:bg-orange-200",
-};
-
-const circleIconButtonStye: { [key in Color]: string } = {
-  blue: "inline-flex items-center me-2 p-2.5 text-sm font-medium text-center text-white rounded-full bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300",
-  red: "inline-flex items-center me-2 p-2.5 text-sm font-medium text-center text-white rounded-full bg-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300",
-  green:
-    "inline-flex items-center me-2 p-2.5 text-sm font-medium text-center text-white rounded-full bg-green-500 hover:bg-blue-green focus:ring-4 focus:outline-none focus:ring-green-300",
-  orange:
-    "inline-flex items-center me-2 p-2.5 text-sm font-medium text-center text-white rounded-full bg-orange-500 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300",
-};
+export function Button({
+  type = "button",
+  label,
+  disabled = false,
+  onClick,
+  full,
+  color,
+}: ButtonProps) {
+  const cn = "hoge";
+  return (
+    <button type={type} className={cn} disabled={disabled} onClick={onClick}>
+      {label}
+    </button>
+  );
+}
 
 export function LoadingButton({
-  label,
-  onClick,
   type = "button",
-  color,
+  label,
+  disabled = false,
+  onClick,
   full,
+  color,
   isProcessing,
-  disabled,
-}: {
-  label: string;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  type: "button" | "submit";
-  full?: true;
-  color: Color;
-  isProcessing: boolean;
-  disabled: boolean;
-}) {
-  const css = full ? buttonFullLinkStye[color] : buttonLinkStye[color];
-
+}: LoadingButtonProps) {
+  const cn = "hoge";
   return (
-    <button type={type} className={css} disabled={disabled} onClick={onClick}>
+    <button type={type} className={cn} disabled={disabled} onClick={onClick}>
       {label}
       {isProcessing && (
         <AiOutlineLoading3Quarters className="inline-block h-6 w-6 ml-3 animate-spin" />
@@ -57,15 +44,34 @@ export function LoadingButton({
   );
 }
 
-export function CircleIconButton({
+export function IconButton({
+  type = "button",
+  label,
+  disabled = false,
+  onClick,
+  full,
   color,
   icon,
-  text,
-  onClick,
-}: CircleIconButtonProps) {
-  const css = circleIconButtonStye[color];
+}: IconButtonProps) {
+  const cn = "hoge";
   return (
-    <button type="button" className={css} onClick={onClick} title={text}>
+    <button type={type} className={cn} disabled={disabled} onClick={onClick}>
+      {icon}
+      <span className="align-middle">{label}</span>
+    </button>
+  );
+}
+
+export function CircleIconButton({
+  type = "button",
+  disabled = false,
+  onClick,
+  color,
+  icon,
+}: CircleIconButtonProps) {
+  const cn = "hoge";
+  return (
+    <button type={type} className={cn} disabled={disabled} onClick={onClick}>
       {icon}
     </button>
   );
--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,7 +1,50 @@
 import { z } from "zod";
 
-export type Color = "blue" | "red" | "green" | "orange";
+export type Color =
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "fuchsia"
+  | "pink"
+  | "rose"
+  | "slate"
+  | "gray"
+  | "zinc"
+  | "neutral"
+  | "stone";
 
+// Button
+export type ButtonProps = {
+  type: "button" | "submit";
+  label: string;
+  disabled?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  full?: true;
+  color?: Color;
+};
+
+export type LoadingButtonProps = ButtonProps & {
+  isProcessing: boolean;
+};
+
+export type IconButtonProps = ButtonProps & {
+  icon: React.ReactNode;
+};
+
+export type CircleIconButtonProps = Omit<IconButtonProps, "label" | "full">;
+
+// Link
 export type ButtonLinkProps = {
   color: Color;
   href: string;
@@ -11,23 +54,4 @@ export type ButtonLinkProps = {
 
 export type IconButtonLinkProps = ButtonLinkProps & {
   icon: React.ReactNode;
-};
-
-export type IconButtonProps = Pick<
-  IconButtonLinkProps,
-  "color" | "text" | "icon"
-> & {
-  onClick: () => void;
-};
-
-export type BottomNavigationIconLinkType = Pick<
-  IconButtonLinkProps,
-  "href" | "text" | "icon"
->;
-
-export type CircleIconButtonProps = {
-  color: Color;
-  icon: React.ReactNode;
-  text: string;
-  onClick: () => void;
 };
