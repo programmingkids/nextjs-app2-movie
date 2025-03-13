00841bb97c466c21715d9db6fca1b0f77d54173b
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2025-03-13 06:52:00 +0000

    added add video to playlist on related video

--- src/components/youtube/youtubePlayerRelatedVideo.tsx
+++ src/components/youtube/youtubePlayerRelatedVideo.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { type YoutubePlayerRelatedVideoProps } from "@/types/index";
+import { Add } from "@/components/playlist/add";
 
 export function YoutubePlayerRelatedVideo({
   videoId,
@@ -9,15 +10,15 @@ export function YoutubePlayerRelatedVideo({
   setCurrentPlay,
 }: YoutubePlayerRelatedVideoProps) {
   const imageStyle = current
-    ? "rounded-xl w-full cursor-pointer border border-[6px] border-red-500"
-    : "rounded-xl w-full cursor-pointer border border-[6px] border-white";
+    ? "rounded-xl w-full col-span-3 cursor-pointer border border-[6px] border-red-500"
+    : "rounded-xl w-full col-span-3 cursor-pointer border border-[6px] border-white";
 
   function handleClickToPlay() {
     setCurrentPlay(index);
   }
 
   return (
-    <div className="grid grid-cols-2 gap-3 mb-3">
+    <div className="grid grid-cols-7 gap-3 mb-3">
       <Image
         src={`https://img.youtube.com/vi/${videoId}/mqdefault.jpg`}
         width={320}
@@ -26,8 +27,11 @@ export function YoutubePlayerRelatedVideo({
         className={imageStyle}
         onClick={handleClickToPlay}
       />
-      <div className="text-3xl lg:text-base">
+      <div className="text-3xl col-span-3 lg:text-base">
         {title.length > 50 ? title.substring(0, 50) + "..." : title}
+      </div>
+      <div>
+        <Add {...{ videoId, title }} />
       </div>
     </div>
   );
